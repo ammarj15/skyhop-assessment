10453897726f2f7a591a8fed1f24c6cea986081e
Ammar Jivraj <ammarj15@gmail.com>
2024-02-13 16:52:51 -0500

    porject complete/files are able to be uploaded

--- src/DocumentUploadModal.jsx
+++ src/DocumentUploadModal.jsx
@@ -4,7 +4,6 @@ import './components/Header';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
-import './styles/modal.scss';
 
 export const DocumentUploadModal = () => {
   return (
